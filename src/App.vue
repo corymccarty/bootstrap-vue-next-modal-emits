028d5f5100017e04bdf00f26c1e67a9ca474043a
Cory McCarty <fenianemt@gmail.com>
2023-12-01 09:50:12 -0700

    First commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { useModal } from 'bootstrap-vue-next'
+import { ref } from 'vue'
+
+const { show } = useModal('test-modal')
+const showModal = ref(false)
+</script>
+
+<template>
+  <BButton @click="show()">Show Modal</BButton>
+  <BModal
+    id="test-modal"
+    v-model="showModal"
+    @show="console.debug('Test Modal: show')"
+    @shown="console.debug('Test Modal: shown')"
+    @hide="console.debug('Test Modal: hide')"
+    @hidden="console.debug('Test Modal: hidden')"
+  ></BModal>
+</template>
